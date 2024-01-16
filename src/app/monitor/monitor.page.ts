eb8ca6ed563b6174763ccd86f2468e4b2e9431e7
Zinderq <nik.melnychenko@gmail.com>
2024-01-16 12:02:15 +0200

    first commit

---
+++ src/app/monitor/monitor.page.ts
@@ -0,0 +1,25 @@
+import { Component, AfterViewInit} from '@angular/core';
+
+@Component({
+  selector: 'app-monitor',
+  templateUrl: './monitor.page.html',
+  styleUrls: ['./monitor.page.scss'],
+})
+export class MonitorPage implements AfterViewInit {
+
+  constructor() { }
+
+  ngAfterViewInit() {
+    this.loadScript('https://public.tableau.com/javascripts/api/viz_v1.js');
+  }
+
+  loadScript(url: string) {
+    const body = <HTMLDivElement> document.body;
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.src = url;
+    script.async = false;
+    script.defer = true;
+    body.appendChild(script);
+  }
+}
---
+++ src/app/reports/reports.page.ts
@@ -0,0 +1,25 @@
+import { Component, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reports',
+  templateUrl: './reports.page.html',
+  styleUrls: ['./reports.page.scss'],
+})
+export class ReportsPage implements AfterViewInit {
+
+  constructor() { }
+
+  ngAfterViewInit() {
+    this.loadScript('https://public.tableau.com/javascripts/api/viz_v1.js');
+  }
+
+  loadScript(url: string) {
+    const body = <HTMLDivElement> document.body;
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.src = url;
+    script.async = false;
+    script.defer = true;
+    body.appendChild(script);
+  }
+}
